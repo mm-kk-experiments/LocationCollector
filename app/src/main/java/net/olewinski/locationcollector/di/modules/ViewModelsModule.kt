179a6662e41a5c43a0d48d022a4873418b21d008
Maciej Olewiński <maciej.olewinski@gmail.com>
2020-12-14 01:28:00 +0100

    Adding maps showing collected location points to the app

--- app/src/main/java/net/olewinski/locationcollector/di/modules/ViewModelsModule.kt
+++ app/src/main/java/net/olewinski/locationcollector/di/modules/ViewModelsModule.kt
@@ -6,7 +6,6 @@ import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.core.component.KoinApiExtension
 import org.koin.dsl.module
 
-@OptIn(KoinApiExtension::class)
 val viewModelsModule = module {
     viewModel { OnboardingViewModel() }
     viewModel { MainFragmentViewModel(get(), get()) }
--- app/src/main/java/net/olewinski/locationcollector/ui/screens/MainFragment.kt
+++ app/src/main/java/net/olewinski/locationcollector/ui/screens/MainFragment.kt
@@ -1,20 +1,55 @@
 package net.olewinski.locationcollector.ui.screens
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import net.olewinski.locationcollector.R
 import net.olewinski.locationcollector.databinding.FragmentMainBinding
 import net.olewinski.locationcollector.viewmodels.MainFragmentViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
+
 
 class MainFragment : Fragment() {
 
     private val mainFragmentViewModel: MainFragmentViewModel by viewModel()
 
     private lateinit var fragmentMainBinding: FragmentMainBinding
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        mainFragmentViewModel.allLocationData.observe(viewLifecycleOwner) { allLocationData ->
+            if (allLocationData.isNotEmpty()) {
+                val builder = LatLngBounds.Builder()
+
+                allLocationData.forEach { locationData ->
+                    val marker = googleMap.addMarker(
+                        MarkerOptions().position(
+                            LatLng(
+                                locationData.latitude,
+                                locationData.longitude
+                            )
+                        )
+                    )
+
+                    builder.include(marker.position)
+                }
+
+                val bounds = builder.build()
+
+                val padding = 0 // offset from edges of the map in pixels
+
+                val cu = CameraUpdateFactory.newLatLngBounds(bounds, padding)
+                googleMap.animateCamera(cu)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,10 +65,7 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mainFragmentViewModel.allLocationData.observe(viewLifecycleOwner) { result ->
-            result.forEach { locationData ->
-                Log.d("MainFragment", locationData.toString())
-            }
-        }
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 }
